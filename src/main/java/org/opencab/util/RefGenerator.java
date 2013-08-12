5a032083c90050044624bfe9b050762d5558468c
rajendersaini <rajendersaini@gmail.com>
2013-08-11 23:04:13 -0500

    added booking form

--- src/main/java/org/opencab/util/RefGenerator.java
+++ src/main/java/org/opencab/util/RefGenerator.java
@@ -7,7 +7,6 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
