33546f09b323032b30624d3089affbea5b25da01
pappulala <pappu@tPPU.XOM>
2013-10-22 13:48:52 -0700

    Playing with spring controller Tests

--- src/test/java/org/opencab/web/controller/LoginControllerTest.java
+++ src/test/java/org/opencab/web/controller/LoginControllerTest.java
@@ -1,12 +1,34 @@
 package org.opencab.web.controller;
 
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Ignore;
 import org.junit.Test;
-import org.opencab.service.BaseTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
-public class LoginControllerTest extends BaseTest {
+public class LoginControllerTest extends BaseControllerTest {
 
-	@Test
-	public void emptyTest() {
+	@Ignore
+	
+	public void loginTest() throws Exception {
 
+		ResultActions res =  mockMvc.perform(get("/auth/login").accept(MediaType.TEXT_HTML));
+		
+		MvcResult mcres =  res.andReturn();
+		
+
+		System.out.println(mcres.getResponse().getForwardedUrl());
+		System.out.println(mcres.getResponse().getContentAsString());
+		System.out.println(mcres.getResponse().getContentType());
+				
 	}
 }
+
